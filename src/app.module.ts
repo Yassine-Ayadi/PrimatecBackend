c23288efe9a19b66cd86617bbe020a49eafbb3db
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2021-07-29 01:42:05 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,21 @@ import { StudentController } from './student/student.controller';
 import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [StudentModule,TypeOrmModule],
+  imports: [StudentModule,
+   TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',//process.env.DB_HOST,
+      port: 3306, //parseInt(process.env.DB_PORT),
+      username: 'root', //process.env.DB_USERNAME,
+      password: '',// process.env.DB_PASSWORD,
+      database: 'nest-student',//process.env.DB_NAME,
+      entities: ["dist/**/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    })
+  ],
   
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
